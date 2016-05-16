3f46433be2e8283bd1d54e89f70a9009bc910460
Sazzad Hossain Khan <itsazzad@gmail.com>
2016-05-17 00:29:53 +0600

    Done

--- code.php
+++ code.php
@@ -1,54 +1,66 @@
 <?php
-$encodeString = "abcdefghijklmnopqrstuvwxyz";
+$baseString = "abcdefghijklmnopqrstuvwxyz";
+echo "\nBase string: $baseString";
 
 function generateKey(){
-    global $encodeString;
-    $key=$encodeString;
-    $key=str_split($key);
+    global $baseString;
+    $key = $baseString;
+    $key = str_split($key);
     shuffle($key);
     return $key;
 }
-
-function encodeString($string,$key){
-    global $encodeString;
+function encodeString($string, $key){
+    global $baseString;
+    $string = str_split($string);
     $key = array_flip($key);
-    $string=str_split($string);
     foreach($string as $i => $letter){
-        $upper=ctype_upper($string[$i]);
         if(ctype_alpha($letter)){
-            //echo $key[strtolower($letter)];
-            $string[$i]=$encodeString[$key[strtolower($letter)]];
-        }
-        if($upper){
-            $string[$i]=strtoupper($string[$i]);
+	        $upper = ctype_upper($string[$i]);
+            $string[$i] = $baseString[$key[strtolower($letter)]];
+	        if($upper){
+	            $string[$i] = strtoupper($string[$i]);
+	        }
         }
     }
-    return $string;
+	/*echo "\nEncoded string array: ";
+	print_r($string);*/
+    return implode($string);
 }
-function decodeString($string,$key){
-    global $encodeString;
-    print_r($encodeString);
-    print_r($string);
-    print_r($key);
+function decodeString($string, $key){
+    global $baseString;
+    $baseSplittedFlipped = str_split($baseString);
+    $baseSplittedFlipped = array_flip($baseSplittedFlipped);
+    $string = str_split($string);
     foreach($string as $i => $letter){
-        $upper=ctype_upper($string[$i]);
         if(ctype_alpha($letter)){
-            echo $key[strtolower($letter)];
-            $string[$i]= $encodeString[$key[strtolower($letter)]];
-        }
-        if($upper){
-            $string[$i]=strtoupper($string[$i]);
+	        $upper = ctype_upper($string[$i]);
+            $string[$i] = $key[$baseSplittedFlipped[strtolower($letter)]];
+	        if($upper){
+	            $string[$i] = strtoupper($string[$i]);
+	        }
         }
     }
-    return $string;
+	/*echo "\nDecoded string array: ";
+	print_r($string);*/
+    return implode($string);
 }
 
 $key = generateKey();
+/*echo "Generated key array: ";
+print_r($key);*/
+echo "\nGenerated key: ".implode($key);
 
-$encodedString = encodeString('Hello World',$key);
-echo decodeString($encodedString,$key);
+$input="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...";
+echo "\nInput: $input";
 
+$encodedString = encodeString($input, $key);
+echo "\nEncoded: ".$encodedString;
 
+$decodedString = decodeString($encodedString, $key);
+echo "\nDecoded: ".$decodedString;
 
-
-
+if($input==$decodedString){
+	echo "\nSuccessfully decoded!!!";
+}else{
+	echo "\n!!!Decoding failed";
+}
